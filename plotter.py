841affb56913d176a0e3bd1b9b6fac827b4e8360
AStaniszewski <staniszewski01adam@gmail.com>
2024-05-10 14:02:45 +0200

    plotter

--- plotter.py
+++ plotter.py
@@ -4,5 +4,6 @@ This module plot data for chosen company
 
 import pandas as pd
 
-def plot_data_from_column(data: pd.DataFrame, col: pd.Series) -> None:
-    data[col].plot()
+def plot_data_from_column(data: str, separator: str, col: str) -> None:
+    df: pd.DataFrame = pd.read_csv(data, sep=separator)
+    df[col].plot()
