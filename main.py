63aecdcce312b5552600e3f1e46d940bcb3d2c20
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-05-10 13:40:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     my_root = "./Dane"
     my_filename = "dane_close.csv"
 
-    print(Data(my_root, my_filename).read_csv_from_root())
+    print(Data(my_root, my_filename).read_csv_from_root().head())
 
 if __name__ == '__main__':
     main()
