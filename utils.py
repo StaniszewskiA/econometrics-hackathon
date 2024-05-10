42a192e01d81274b3333b14acfc6f3becc70fba7
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-05-10 13:42:33 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+class Utils:
+    def __init__(self):
+        pass
+
+    def __repr__(self) -> str:
+        "Klasa do pracy na danych w plikach, głównie wykresy"
+
+    def create_plot():
+        pass
